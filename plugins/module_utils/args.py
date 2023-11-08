01df3dba6879d7b36627f0df416ffe83e9e84ed7
Guido Grazioli <ggraziol@redhat.com>
2023-11-08 16:57:19 +0100

    initial

---
+++ plugins/module_utils/args.py
@@ -0,0 +1,22 @@
+from __future__ import (absolute_import, division, print_function)
+
+
+__metaclass__ = type
+
+
+def common_args() -> dict:
+    return dict(
+        platform=dict(type='str', required=False),
+        kubeconfig=dict(type='str', required=False),
+        context=dict(type='str', required=False),
+        namespace=dict(type='str', required=False),
+        hostname=dict(type='str', required=False),
+        podman_endpoint=dict(type='str', required=False),
+    )
+
+
+def add_fact(result, d):
+    facts = result['ansible_facts'] if 'ansible_facts' in result else dict()
+    facts.update(d)
+    result['changed'] = True
+    result['ansible_facts'] = facts
---
+++ plugins/module_utils/sites.py
@@ -0,0 +1,20 @@
+from __future__ import (absolute_import, division, print_function)
+
+
+__metaclass__ = type
+
+
+def get_sites(hostvars) -> list[dict]:
+    # map site entry by site id
+    sites = list[dict]()
+    for host in hostvars:
+        hostvar = hostvars[host]
+        if 'site' in hostvar:
+            try:
+                site = hostvar['site']
+                sites.append(site)
+            except Exception as ex:
+                raise RuntimeError('Unable to load site for %s - %s' % (host, ex.__str__()))
+        else:
+            raise RuntimeError('site information has not been loaded for: %s' % host)
+    return sites
